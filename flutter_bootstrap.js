463623c3390e09540270e0d1f995fbab0cbf84dd
Suat Martin <sgiraygonzal@umass.edu>
2025-03-11 12:26:48 -0400

    flutter app build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "951219060"
+    serviceWorkerVersion: "2257173184"
   }
 });
