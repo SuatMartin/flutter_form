af322e00eaab640df82b28ab610b41433c5b7976
Suat Martin <sgiraygonzal@umass.edu>
2025-04-15 11:13:08 -0400

    cambios esteticos

--- lib/screens/google_form_modal.dart
+++ lib/screens/google_form_modal.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class GoogleFormModal extends StatefulWidget {
@@ -67,4 +67,4 @@ class _GoogleFormModalState extends State<GoogleFormModal> {
       ),
     );
   }
-}
+} */
